1f6ec089d20b07a75fa655df151612c077a0c7fb
jasaga2401 <jasaga@protonmail.com>
2023-11-20 13:19:56 +0000

    Add project files.

---
+++ if_statements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare a variable
+        int number = 10;
+
+        // Check if the number is greater than 5
+        if (number > 5)
+        {
+            Console.WriteLine("The number is greater than 5.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not greater than 5.");
+        }
+    }
+}
+
